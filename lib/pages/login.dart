758460125b80199d566687c9b73b99aa8a2ee9aa
kpark0401 <kpark0113@gmail.com>
2024-05-29 23:12:04 +0900

    login_failed

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class Login extends StatefulWidget {
@@ -15,40 +14,41 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Expanded(
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
                 InkWell(
-                  onTap: () {
-                    signInWithGoogle();
-                  },
-                  child: Card(
-                    margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(7)),
-                    elevation: 2,
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset('assets/google.png'),
-                          const SizedBox(
-                            width: 10,
-                          ),
-                          const Text('Sign In With Google',
-                              style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 17,
-                              )),
-                        ]),
-                  ),
-                ),
-              ]))
-        ],
-      )),
+                    onTap: () {
+                      signInWithGoogle();
+                    },
+                    child: Card(
+                      margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(7)),
+                      elevation: 2,
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset('assets/google.png'),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            const Text(
+                              "Sign In With Google",
+                              style:
+                                  TextStyle(color: Colors.grey, fontSize: 17),
+                            )
+                          ]),
+                    )),
+              ],
+            ))
+          ],
+        ),
+      ),
     );
   }
 
